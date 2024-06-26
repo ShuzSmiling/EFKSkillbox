1c8af47c902010e68537d0e404dcfeb83f1f9e11
Alex Ch <shuzsmiling@gmail.com>
2024-06-26 20:39:09 +0300

    Add dock files

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/one')
+def one():
+    return 'one', 201
+
+
+@app.route('/two')
+def two():
+    return 'two', 202
+
+
+@app.route('/three')
+def three():
+    return 'three', 203
+
+
+@app.route('/four')
+def four():
+    return 'four', 204
+
+
+@app.route('/five')
+def five():
+    return 'five', 205
+
+
+@app.route('/error')
+def error():
+    return 'error', 500
+
+
+if __name__ == '__main__':
+    app.run()
+
